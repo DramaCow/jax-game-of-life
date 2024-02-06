6f3cf5cfd20f8351d4ef9ba33b2f5a818d2d38f2
Michael Beukman <michael.beukman@gmail.com>
2024-02-06 16:46:40 +0000

    Added explicit numpy conversion

--- game_of_life.py
+++ game_of_life.py
@@ -1,26 +1,25 @@
-import numpy as np
 import pygame
 import jax
 import jax.numpy as jnp
 from jax.scipy.signal import convolve
 from functools import partial
-
+import numpy as np
 class Canvas(object):
     def __init__(self):
         self.screen = None
         self.clock = None
-
+        
     def render(self, image):
         width_px, height_px, _ = image.shape
-
+        
         if self.screen is None:
             pygame.init()
             pygame.display.init()
             self.screen = pygame.display.set_mode((width_px, height_px))
-
+            
         if self.clock is None:
             self.clock = pygame.time.Clock()
-
+        
         surface = pygame.surfarray.make_surface(np.asarray(image))
 
         assert self.screen is not None
@@ -33,7 +32,7 @@ class Canvas(object):
         if self.screen is not None:
             pygame.display.quit()
             pygame.quit()
-
+         
 @jax.jit
 def update(cells):
     counts = convolve(jnp.pad(cells, 1, mode='wrap'), jnp.array([
